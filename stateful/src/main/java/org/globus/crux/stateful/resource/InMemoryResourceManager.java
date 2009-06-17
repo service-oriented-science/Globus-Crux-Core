334842949073ba3a236eb3df67eeaf26aba96daa
Tom Howe <turtlebender@gmail.com>
2009-06-17 20:43:02 +0000

    updated additional javadoc

--- stateful/src/main/java/org/globus/crux/stateful/resource/InMemoryResourceManager.java
+++ stateful/src/main/java/org/globus/crux/stateful/resource/InMemoryResourceManager.java
@@ -13,16 +13,38 @@ import java.util.concurrent.ConcurrentHashMap;
 public class InMemoryResourceManager<T, V> implements ResourceManager<T, V> {
     private ConcurrentHashMap<T, V> map = new ConcurrentHashMap<T, V>();
 
+    /**
+     * Find a resource based on the key.
+     *
+     * @param key The key of the resource
+     * @return The key associated with the resource.
+     * @throws ResourceException If the lookup fails.
+     */
     public V findResource(T key) throws ResourceException {
         return map.get(key);
     }
 
+    /**
+     * Remove the resource associated with the key.  This will permanently remove the resource.
+     *
+     * @param key The key of the resource.
+     * @return The removed resource.
+     * @throws ResourceException If the removal fails.
+     */
     public V removeResource(T key) throws ResourceException {
         V result = map.get(key);
         map.remove(key);
         return result;
     }
 
+    /**
+     * Store the resource and the associated key.  This should only be used on resources that do
+     * not already exist.  Throws an exception if they key already exists.
+     *
+     * @param key The key to store.
+     * @param resource The resource to store.
+     * @throws ResourceException If the store operation fails.
+     */
     public void storeResource(T key, V resource) throws ResourceException {
         if (!map.contains(key)) {
             map.put(key, resource);
@@ -31,6 +53,15 @@ public class InMemoryResourceManager<T, V> implements ResourceManager<T, V> {
         }
     }
 
+    /**
+     * Update the resource in the resource manager.  This should only be used on resources that
+     * do already exist.  Throws an exception if the key does not already exist in the manager.
+     *
+     * @param key The key to store.
+     * @param resource The resource to store.
+     * @return The updated resource.
+     * @throws ResourceException If the update operation fails.
+     */
     public V updateResource(T key, V resource) throws ResourceException {
         if (!map.contains(key)) {
             throw new ResourceException("Key does not exist in map");
--- stateful/src/main/java/org/globus/crux/stateful/utils/InjectionUtils.java
+++ stateful/src/main/java/org/globus/crux/stateful/utils/InjectionUtils.java
@@ -7,13 +7,26 @@ import java.security.PrivilegedAction;
 import java.security.AccessController;
 import java.lang.reflect.Field;
 
-
+/**
+ * Utilities for injecting values into fields and methods.
+ *
+ * @author Tom Howe
+ * @since 1.0
+ * @version 1.0
+ */
 public final class InjectionUtils {
     private static Logger logger = LoggerFactory.getLogger(InjectionUtils.class);
 
     private InjectionUtils() {
     }
 
+    /**
+     * Inject value into field
+     *
+     * @param f The field into which to inject the value
+     * @param o The Object into which to inject the value
+     * @param v The field to inject
+     */
     @SuppressWarnings("unchecked")
     public static void injectFieldValue(final Field f,
                                         final Object o,

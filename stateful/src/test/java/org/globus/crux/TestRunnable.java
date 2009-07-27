7fa1f711f7a58d566d24d4453d860aaca020dbb1
Tom Howe <turtlebender@gmail.com>
2009-07-27 17:59:02 +0000

    refactored test classes

---
+++ stateful/src/test/java/org/globus/crux/TestRunnable.java
@@ -0,0 +1,10 @@
+package org.globus.crux;
+
+/**
+ * @author turtlebender
+ *         User: turtlebender
+ *         Date: Jun 16, 2009
+ *         Time: 1:11:51 PM
+ */
+public class TestRunnable {
+}

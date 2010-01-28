1c0ae29faa853649161aad0f69c8fc91af0c0795
dseider <doreen.seider@dlr.de>
2010-01-28 16:36:04 -0600

    remove Eclipse dependent files, extend pom to generate Manifest and Eclipse project files

--- org.globus.crux.test/src/main/java/org/globus/crux/test/DummyInterceptor.java
+++ org.globus.crux.test/src/main/java/org/globus/crux/test/DummyInterceptor.java
@@ -1,19 +1,14 @@
 package org.globus.crux.test;
 
-import org.apache.cxf.binding.soap.SoapMessage;
-import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.message.Message;
 
 public class DummyInterceptor implements Interceptor {
 
-	public void handleMessage(SoapMessage message) throws Fault {
+	public void handleFault(Message message) {
 	}
 
-	public void handleFault(Message arg0) {
-	}
-
-	public void handleMessage(Message arg0) throws Fault {
+	public void handleMessage(Message arg0) {
 	}
 
 }
